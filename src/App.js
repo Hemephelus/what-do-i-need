5e334372970b7f713636874391bb8b3f192f3c5a
Hemephelus <nwachukwuujubuonu@gmail.com>
2023-01-24 22:05:53 +0100

    mobile view done

--- src/App.js
+++ src/App.js
@@ -1,114 +1,133 @@
-import summaryIcon from "./images/home-page/Summary Icon.png"
-import BreakDownIcon from "./images/home-page/Break Down.png"
-import ComprehensiveIcon from "./images/home-page/Comprehensive Icon.png"
-import PrintPreviewIcon from "./images/home-page/Print Preview.png"
-import './App.css';
-import Typical from 'react-typical'
+import summaryIcon from "./images/home-page/Summary Icon.png";
+import BreakDownIcon from "./images/home-page/Break Down.png";
+import ComprehensiveIcon from "./images/home-page/Comprehensive Icon.png";
+import PrintPreviewIcon from "./images/home-page/Print Preview.png";
+import "./App.css";
+import Typical from "react-typical";
 
 function App() {
   return (
-    <div className="App">
-    <div className='App-header'>
-      <h1 className='App-name'>WDIN</h1>
-      <div className='header-links'>
-        <p className='header-link'>Add School</p>
-        <p className='header-link'>Guide</p>
-        <p className='header-link'>Community</p>
-      </div>
-    </div>
-    
-    <div className='App-body'>
-    <div className='Hero-section'>
-    <div>
-     <h1 className='Hero-headline'>
-      A CGPA Calculator
-      </h1>
-      <div>
-      <h3 className='Hero-sub-headline'>
-      Find out what you need to be a  
-      <Typical
-        steps={['first class student.', 2000, 'second class student.', 2000, 'third class student.', 2000]}
-        loop={Infinity}
-        wrapper="p"
-      />
-      </h3>
-      </div>
-     </div>
-      <div className='Hero-buttons'>
-        <button className='Primary-button'>Summary Calculation</button>
-        <button className='Secondary-button'>Comprehensive Calculation</button>
-      </div>
-    </div>
-
-
-    <div className='feature-section'>
-    <div className='feature-headline'>
-     <h1 className='feature-title'>
-     Features
-      </h1>
-      <div className='feature-line'></div>
-     </div>
-      <div className='features'>
-      <div className='feature'>
-      <figure>
-      <img className="summary-icon" src={summaryIcon} alt={"summary icon"}/>
-      </figure>
-      <h1>
-      Summary
-      </h1>
-      <p>
-      Quickly determine what your would need to be on a first class.
-      </p>
+    <div className="flex flex-col gap-[80px] App">
+      <div className="flex justify-between App-header">
+        <h1 className="text-[16px] font-semibold App-name">WDIN</h1>
+        <div className="flex gap-[16px] header-links">
+          <p className="text-[12px] font-semibold text-[#6246EA] header-link">
+            Add School
+          </p>
+          <p className="text-[12px] font-semibold text-[#6246EA] header-link">
+            Guide
+          </p>
+          <p className="text-[12px] font-semibold text-[#6246EA]  header-link">
+            Community
+          </p>
+        </div>
       </div>
 
-      <div className='feature'>
-      <figure>
-      <img className="Print-Preview-Icon" src={PrintPreviewIcon} alt={"Print Preview Icon"}/>
-      </figure>
-      <h1>
-      Print Result
-      </h1>
-      <p>
-      Print your summary or comprehensive results for personal use.
-      </p>
-      </div>
-    
-      <div className='feature'>
-      <figure>
-      <img className="Break-Down-Icon" src={BreakDownIcon} alt={"Break Down Icon"}/>
-      </figure>
-      <h1>
-      Break Down
-      </h1>
-      <p>
-      View the quick break down on how GPAs and CGPA are calculated.
-      </p>
+      <div className="flex flex-col gap-[80px] justify-center items-center text-center App-body">
+        <div className="flex flex-col gap-[40px] sm: Hero-section">
+          <div>
+            <h1 className=" text-[32px] font-bold sm: Hero-headline">
+              A CGPA Calculator
+            </h1>
+            <div>
+              <h3 className="text-[16px] Hero-sub-headline">
+                Find out what you need to be a
+                <Typical
+                  steps={[
+                    "first class student.",
+                    2000,
+                    "second class student.",
+                    2000,
+                    "third class student.",
+                    2000,
+                  ]}
+                  loop={Infinity}
+                  wrapper="p"
+                />
+              </h3>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-[16px] Hero-buttons">
+            <button className="bg-[#6246EA] text-[#fffffe] rounded-lg px-[16px] py-[8px] font-semibold Primary-button">
+              Summary Calculation
+            </button>
+            <button className="bg-[#FFFFFE] text-[#6246EA] rounded-lg px-[16px] py-[8px] font-semibold Secondary-button">
+              Comprehensive Calculation
+            </button>
+          </div>
+        </div>
 
-      </div>
-      <div className='feature'>
-      <figure>
-      <img className="Comprehensive-Icon" src={ComprehensiveIcon} alt={"Comprehensive Icon"}/>
-      </figure>
-      <h1>
-      Break Down
-      </h1>
-      <p>
-      View the quick break down on how GPAs and CGPA are calculated.
-      </p>
-      </div>
-  
-      </div>
-    </div>
+        <div className="flex flex-col gap-[40px] items-center feature-section">
+          <div className="flex flex-col gap-[8px] items-center feature-headline">
+            <h1 className="text-[20px] font-bold feature-title">Features</h1>
+            <div className="w-[50px] h-[5px] bg-[#6246EA] feature-line"></div>
+          </div>
+          <div className="flex  flex-col gap-[40px] features">
+            <div className="feature">
+              <figure>
+                <img
+                  className="summary-icon"
+                  src={summaryIcon}
+                  alt={"summary icon"}
+                />
+              </figure>
+              <h1>Summary</h1>
+              <p>
+                Quickly determine what your would need to be on a first class.
+              </p>
+            </div>
 
-    </div>
-    <div className='App-footer'>
-    <h1 className='App-name'>WDIN</h1>
-    <div className='footer-links'>
-      <p className='footer-link'>Add Your School</p>
-      <p className='footer-link'>Guide</p>
-      <p className='footer-link'>Community</p>
-    </div>
-    </div>
+            <div className="flex flex-col items-center feature">
+              <figure>
+                <img
+                  className="Print-Preview-Icon"
+                  src={PrintPreviewIcon}
+                  alt={"Print Preview Icon"}
+                />
+              </figure>
+              <h1>Print Result</h1>
+              <p>
+                Print your summary or comprehensive results for personal use.
+              </p>
+            </div>
+
+            <div className="flex flex-col items-center feature">
+              <figure>
+                <img
+                  className="Break-Down-Icon"
+                  src={BreakDownIcon}
+                  alt={"Break Down Icon"}
+                />
+              </figure>
+              <h1>Break Down</h1>
+              <p>
+                View the quick break down on how GPAs and CGPA are calculated.
+              </p>
+            </div>
+            <div className="flex flex-col items-center feature">
+              <figure>
+                <img
+                  className="Comprehensive-Icon"
+                  src={ComprehensiveIcon}
+                  alt={"Comprehensive Icon"}
+                />
+              </figure>
+              <h1>Break Down</h1>
+              <p>
+                View the quick break down on how GPAs and CGPA are calculated.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-[16px] App-footer ">
+        <h1 className="text-[16px] font-semibold App-name">WDIN</h1>
+        <div className="text-[12px] flex gap-[32px] text-[#FFFFFE] footer-links">
+          <p className="footer-link">Add Your School</p>
+          <p className="footer-link">Guide</p>
+          <p className="footer-link">Community</p>
+        </div>
+      </div>
     </div>
   );
 }
